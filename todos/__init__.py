b216a99ce9752e3acea3113009776610bdd2be66
Tom Slankard <tom@earthmine.com>
2014-12-20 12:00:42 -0800

    Add docstrings, clean up template and CSS

--- todos/__init__.py
+++ todos/__init__.py
@@ -1,3 +1,10 @@
+"""
+todos module
+Tom Slankard <tom.slankard@here.com>
+
+Simple Flask app for managing a todo list.
+"""
+
 import os
 from flask import Flask, render_template, request, redirect, url_for
 from flask.ext.sqlalchemy import SQLAlchemy
@@ -5,11 +12,15 @@ import pkg_resources
 
 app = Flask(__name__)
 db_path = pkg_resources.resource_filename('todos', 'todos.db')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_path 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_path
 db = SQLAlchemy(app)
 
 
 class Todo(db.Model):
+
+    """ORM for a todo item, which in this case,
+    consists only of a string, a completed flag,
+    and an integer primary key."""
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String)
     completed = db.Column(db.Boolean)
@@ -21,11 +32,14 @@ class Todo(db.Model):
 
 @app.route('/')
 def index():
+    """Merely redirects to /todos"""
     return redirect(url_for('todos'))
 
 
 @app.route('/todos', methods=['GET', 'POST'])
 def todos():
+    """Display a list of todos, or create a new todo and display the list
+    depending on whether the form was submitted."""
     if request.method == 'GET':
         return render_template("todos.html", todos=Todo.query.all())
     elif request.method == 'POST':
@@ -33,20 +47,22 @@ def todos():
         db.session.add(new_todo)
         db.session.commit()
         return render_template("todos.html", todos=Todo.query.all())
-    flask.abort(405) # method not allowed
+    flask.abort(405)  # method not allowed
 
 
 @app.route('/todos/<id>/complete')
 def complete_todo(id):
+    """Sets completed to true for the specified todo item."""
     todo = Todo.query.filter_by(id=id).first()
     todo.completed = True
     db.session.add(todo)
     db.session.commit()
     return redirect(url_for('todos'))
 
-    
+
 @app.route('/todos/<id>/uncomplete')
 def uncomplete_todo(id):
+    """Sets completed to false for the specified todo item."""
     todo = Todo.query.filter_by(id=id).first()
     todo.completed = False
     db.session.add(todo)
@@ -56,13 +72,16 @@ def uncomplete_todo(id):
 
 @app.route('/remove_completed')
 def remove_completed():
+    """Removes all completed todo items."""
     todos = Todo.query.filter_by(completed=True).all()
     for todo in todos:
         db.session.delete(todo)
     db.session.commit()
     return redirect(url_for('todos'))
 
+
 def main():
+    """Initialize the database and run the Flask server."""
     db.create_all()
     app.run(debug=True)
 
--- todos/cwd.py
+++
@@ -1,6 +0,0 @@
-import pkg_resources
-
-print pkg_resources.resource_filename('todos', 'todos.db')
-
-
-
